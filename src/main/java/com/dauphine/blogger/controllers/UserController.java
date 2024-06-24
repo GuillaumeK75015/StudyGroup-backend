83d4319b35c9e9279ceea247abaaf8b75c3a6514
GuillaumeK75015 <123181141+GuillaumeK75015@users.noreply.github.com>
2024-06-25 01:10:53 +0200

    register completed

--- src/main/java/com/dauphine/blogger/controllers/UserController.java
+++ src/main/java/com/dauphine/blogger/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.dauphine.blogger.controllers.requestbody.UserDto;
 import com.dauphine.blogger.models.User;
 import com.dauphine.blogger.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,15 +15,17 @@ public class UserController {
 
     @Autowired
     private UserService userService;
-
     @PostMapping("/register")
-    public ResponseEntity<User> register(@RequestBody UserDto userDto) {
+    public ResponseEntity<String> register(@RequestBody UserDto userDto) {
         try {
             User user = userService.registerUser(userDto.username(), userDto.password());
-            return ResponseEntity.status(HttpStatus.CREATED).body(user);
+            return ResponseEntity.status(HttpStatus.CREATED).body("User created successfully.");
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Username already exists.");
         } catch (Exception e) {
             e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while creating the user.");
         }
     }
+
 }
